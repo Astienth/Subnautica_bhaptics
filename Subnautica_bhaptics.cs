4a898c0ad0d1c8abf5903ddaae9c1081be8303cd
Astienth <astienth@gmail.com>
2023-01-28 06:27:26 +0100

    initial commit

---
+++ Subnautica_bhaptics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using MyBhapticsTactsuit;
+
+namespace Subnautica_bhaptics
+{
+    [BepInPlugin("org.bepinex.plugins.Subnautica_bhaptics", "Subnautica bHaptics integration", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+#pragma warning disable CS0109 // Remove unnecessary warning
+        internal static new ManualLogSource Log;
+#pragma warning restore CS0109
+        public static TactsuitVR tactsuitVr;
+
+
+        private void Awake()
+        {
+            // Make my own logger so it can be accessed from the Tactsuit class
+            Log = base.Logger;
+            // Plugin startup logic
+            Logger.LogMessage("Plugin Compound_bhaptics is loaded!");
+            tactsuitVr = new TactsuitVR();
+            // one startup heartbeat so you know the vest works correctly
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+            // patch all functions
+            var harmony = new Harmony("bhaptics.patch.compound");
+            harmony.PatchAll();
+        }
+    }
+    /*
+    [HarmonyPatch(typeof(Food), "OnEat", new Type[] { })]
+    public class bhaptics_OnEat
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (Plugin.tactsuitVr.suitDisabled)
+            {
+                return;
+            }
+            Plugin.tactsuitVr.PlaybackHaptics("Eating");
+        }
+    }
+    */
+}
+
